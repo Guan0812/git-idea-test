0796aa05c59a830b2f58d97e63e7387d6d35ff75
G <@1456579290@qq.com>
2022-03-31 18:00:40 +0800

    第一次提交  dev

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -2,6 +2,7 @@ package cn.tt.git.controller;
 
 public class TestController {
     public static void main(String[] args) {
+        System.out.println("hello Dev");
         System.out.println("hello git");
     }
 }
